fd3689d05282b3cb36b22534cfad9c40f559d07a
magic891103 <133174388+magic891103@users.noreply.github.com>
2023-06-17 00:54:06 +0000

    magic891103-domain-name-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["I", "You", "She", "her", "He", "us"];
+  let adjs = ["enormous", "great", "silly", "fun", "fast"];
+  let nouns = ["cat", "dog", "tiger", "lion", "peach", "apple"];
+  let tlds = [".com", ".net", ".org", ".gov", "mil", ".edu"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${pronoun}${adj}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
